6f954b9b0009a3b05975d3bd5ec8084c77ac5053
leofengg <leofengg02@gmail.com>
2023-08-03 20:29:05 -0600

    finished menubar

--- client/src/components/MenuBar.js
+++ client/src/components/MenuBar.js
@@ -3,13 +3,16 @@ import { Menu } from 'semantic-ui-react'
 import {Link} from 'react-router-dom'
 
 function MenuBar() {
-  const [activeItem, setActiveItem] = useState('')
+  
+  const pathname = window.location.pathname;
+  const path = pathname === '/' ? 'home' : pathname.substring(1);
+  const [activeItem, setActiveItem] = useState(path);
   
   const handleItemClick = (e, { name }) => setActiveItem(name);
  
     return (
 
-        <Menu pointing secondary>
+        <Menu pointing secondary size='massive' color='teal'>
           <Menu.Item
             name='home'
             active={activeItem === 'home'}
