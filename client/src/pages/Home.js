96d043c6cbe2da52b0c5aa5d20724be099d19ee2
leofengg <leofengg02@gmail.com>
2023-08-03 21:39:33 -0600

    can't retrieve queries, come out undefined

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,24 @@
 import React from 'react'
+import {useQuery} from '@apollo/react-hooks'
+import gql from 'graphql-tag';
+
+const FETCH_POSTS_QUERY = gql`
+  query {
+      getPosts {
+      id 
+    }
+  }
+` 
 
 export default function Home() {
+  const {loading,error, data} = useQuery(FETCH_POSTS_QUERY);
+  console.log(FETCH_POSTS_QUERY);
+
+  // if (data) {
+  //   console.log(data);
+  // }
   return (
     <div><h1>HOME</h1></div>
   )
-}
+}  
+
